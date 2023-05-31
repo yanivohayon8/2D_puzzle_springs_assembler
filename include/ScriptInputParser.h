d59e4488d63ffe4b6c0f5c31d8fe5f2177aa4e90
Yaniv Ohayon <yanivohayon8@gmail.com>
2023-05-31 13:14:39 +0300

    set the puzzle path as a parameter

---
+++ include/ScriptInputParser.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+void parseInput(bool& oIsSimulationAuto, std::string &oPuzzleDirectory, int argc, char** argv);
--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,17 @@
 #include <Piece.h>
 #include <World.h>
 #include <VertexMating.h>
+#include <ScriptInputParser.h>
 
 int main(int argc, char** argv)
 {
-	std::string puzzleDirectory = "../data/ofir/RePAIR/group_39";
 	//std::string puzzleDirectory = "../data/ofir/Pseudo-Sappho_MAN_Napoli_Inv9084/Puzzle1/0";
+	//std::string puzzleDirectory = "../data/ofir/RePAIR/group_39";
+	std::string puzzleDirectory; 
+	bool isSimulationAuto = true;
 	bool isOfir = true;
+	parseInput(isSimulationAuto,puzzleDirectory,argc,argv);
+
 	DataLoader dataLoader(puzzleDirectory);
 	std::vector<Piece> pieces;
 	dataLoader.loadPieces(pieces, isOfir);
