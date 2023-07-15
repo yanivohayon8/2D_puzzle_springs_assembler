bae42108eb41fd23bd26ca66648327d59c3b9283
Yaniv Ohayon <yanivohayon8@gmail.com>
2023-07-15 11:48:09 +0300

    fix initi bug

--- include/Puzzle.h
+++ include/Puzzle.h
@@ -11,17 +11,17 @@ class Puzzle
 private:
 	std::vector<Piece> pieces_;
 	std::map<std::string, Piece> id2piece_;
-	std::vector<VertexMating*> groundTruthMatings_;
+	std::vector<VertexMating> groundTruthMatings_;
 	
 public:
 	Puzzle();
-	std::vector<VertexMating*>* getGroundTruthMatings();
+	std::vector<VertexMating>* getGroundTruthMatings();
 	std::vector<Piece>* getPieces();
-	Puzzle(std::vector<Piece>& pieces, std::vector<VertexMating*> groundTruthMatings);
-	void findPiecesToReconstruct(std::vector<Piece> &oReconstructPieces, std::vector<VertexMating*>& matings);
+	Puzzle(std::vector<Piece>& pieces, std::vector<VertexMating> groundTruthMatings);
+	void findPiecesToReconstruct(std::vector<Piece> &oReconstructPieces, std::vector<VertexMating>& matings);
 
 	void setPieces(std::vector<Piece>& newPieces);
-	void setGroundTruthMatings(std::vector<VertexMating*>& newGroundTruthMatings);
+	void setGroundTruthMatings(std::vector<VertexMating>& newGroundTruthMatings);
 
 };
 
