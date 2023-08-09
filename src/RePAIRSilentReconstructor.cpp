4fc11820132d1b41dda9c9a7364c157c75cdbfc4
Yaniv Ohayon <yanivohayon8@gmail.com>
2023-08-09 14:23:29 +0300

    acheive visual reconstructor results

--- src/RePAIRSilentReconstructor.cpp
+++ src/RePAIRSilentReconstructor.cpp
@@ -1,7 +1,13 @@
 #include "RePAIRSilentReconstructor.h"
 
 
-void RePAIRSilentReconstructor::Run(std::string screenshotPathBeforeCollide = "", std::string screenshotPathAfterCollide = "")
+RePAIRSilentReconstructor::RePAIRSilentReconstructor():SilentReconstructor()
+{
+	isEnableJointsCollide_ = true;
+	jointStartLength_ = 0.05;
+}
+
+void RePAIRSilentReconstructor::Run(std::string screenshotPathBeforeCollide , std::string screenshotPathAfterCollide)
 {
 	if (isDebugScreenVisible_ || screenshotPathBeforeCollide != "" || screenshotPathAfterCollide != "")
 	{
@@ -9,45 +15,18 @@ void RePAIRSilentReconstructor::Run(std::string screenshotPathBeforeCollide = ""
 	}
 
 	int iteration = 0;
+	setPiecesCollisionOn();
 	//int iterationToConvergePerPiece = 1000;
 	int iterationToConverge = activePieces_.size() * iterationToConvergeBeforeCollidePerPiece_;
 	progress(iterationToConverge);
 
-	for (auto& piece : activePieces_)
-	{
-		piece.setCollideOn();
-	}
+	
 
-	int iterationToSecondConverage = activePieces_.size() * iterationToConvergeAfterCollidePerPiece_;
-	progress(iterationToSecondConverage);
+	/*int iterationToSecondConverage = activePieces_.size() * iterationToConvergeAfterCollidePerPiece_;
+	progress(iterationToSecondConverage);*/
 
 
-	// Debug
-	if (screenshotPathAfterCollide != "")
-	{
-		screen_->clearDisplay();
-		auto redColor = sf::Color::Red;
-
-		for (auto& mating : activeMatings_)
-		{
-			auto& anchorA = mating.jointRef_->GetAnchorA();
-			auto& anchorB = mating.jointRef_->GetAnchorB();
-			screen_->drawLine(anchorA, anchorB, redColor, -1);
-		}
-
-		for (auto& piece : activePieces_)
-		{
-			screen_->drawSprite(piece.id_, piece.refb2Body_->GetTransform());
-		}
-
-		screen_->screenShotToFile(screenshotPathAfterCollide);
-	}
-
-
-	if (screenshotPathBeforeCollide != "" || screenshotPathAfterCollide != "")
-	{
-		screen_->closeWindow();
-	}
+	
 
 	if (isDebugScreenVisible_)
 	{
