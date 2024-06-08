8f992a7346c0ace4d3750c98f4da4bbcbbdbef3e
Yaniv Ohayon <yanivohayon8@gmail.com>
2024-06-08 18:38:56 +0300

    simpliying the api

--- src/main.cpp
+++ src/main.cpp
@@ -14,24 +14,16 @@ int main(int argc, char** argv)
 	std::map<std::string, std::string> param2Value;
 	parseInput(param2Value,argc,argv);
 
-	if (param2Value.count("server") > 0)
+	if (param2Value.count("ConvexDrawing"))
 	{
-		if (param2Value.at("server") == "ConvexDrawing")
-		{
-
-			HttpServerCD httpServer;
-			httpServer.run();
-		}
-		else if (param2Value.at("server") == "RePAIR") {
-			HttpServerRePAIR httpServer;
-			httpServer.run();
-		}
+		HttpServerCD httpServer;
+		httpServer.run();
 	}
 	else
 	{
-		std::cout << "Please provide the task you want to execute (test\server)" << std::endl;
+		HttpServerRePAIR httpServer;
+		httpServer.run();
 	}
-	
 
 	return 0;
 }
