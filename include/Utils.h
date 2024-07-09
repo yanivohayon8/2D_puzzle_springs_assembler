8df54362608becfce73efebb084e6635cf985074
Yaniv Ohayon <yanivohayon8@gmail.com>
2024-07-09 09:09:44 +0300

    delete unused functions

--- include/Utils.h
+++ include/Utils.h
@@ -11,8 +11,4 @@ int sampleIntUniformly(double max, double min, int seed);
 
 void getScreenSize(int& oHeight, int& oWidth);
 
-void getRoatationMatrix(Eigen::MatrixX2d& oRotation, double angle);
-
-void getRoatationMatrix(Eigen::MatrixX2d& oRotation, const b2Rot& rot);
-
 bool isFloatString(const std::string str);
--- src/Utils.cpp
+++ src/Utils.cpp
@@ -2,20 +2,6 @@
 
 void generate2DVectors(std::vector<b2Vec2>&vectors,int n,int maxX,int maxY,int padding, int seed)
 {
-	/*std::mt19937 gen(seed); 
-	std::uniform_int_distribution<int> distributionX(padding + 0, maxX-padding);
-	std::uniform_int_distribution<int> distributionY(padding + 0, maxY-padding);
-
-	for (int i = 0; i < n; i++)
-	{
-		int x = distributionX(gen);
-		int y = distributionY(gen);
-		vectors.push_back(b2Vec2(x, y));
-	}*/
-
-
-	
-
 	// Use the Mersenne Twister engine for randomness
 	std::mt19937 gen(seed);
 
@@ -53,24 +39,6 @@ int sampleIntUniformly(double max, double min, int seed)
 	std::uniform_int_distribution<int> distribution(min,max);
 	return distribution(gen);
 }
-
-
-void getRoatationMatrix(Eigen::MatrixX2d& oRotation, double angle)
-{
-	oRotation(0, 0) = std::cos(angle);
-	oRotation(1, 0) = std::sin(angle);
-	oRotation(1, 0) = -std::sin(angle);
-	oRotation(1, 1) = std::cos(angle);
-}
-
-void getRoatationMatrix(Eigen::MatrixX2d& oRotation, const b2Rot& rot)
-{
-	oRotation(0, 0) = rot.c;
-	oRotation(1, 0) = rot.s;
-	oRotation(1, 0) = -rot.s;
-	oRotation(1, 1) = rot.c;
-}
-
 
 bool isFloatString(const std::string str) {
 	try {
