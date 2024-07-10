b6acb230e9ae669190b313a6c0a221ac80c83b1f
Yaniv Ohayon <yanivohayon8@gmail.com>
2024-07-10 16:42:50 +0300

    remove comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,10 @@
 #include "http_server.h"
-//#include "HttpServerCD.h"
-//#include "HttpServerRePAIR.h"
-#include <DataLoader.h>
-#include <Piece.h>
-////#include <World.h>
-#include <VertexMating.h>
-#include <ScriptInputParser.h>
-#include <Puzzle.h>
-#include <reconstruction.h>
+
 
 int main(int argc, char** argv)
 {
 	std::map<std::string, std::string> param2Value;
-	parseInput(param2Value,argc,argv);
-
-	/*if (param2Value.count("ConvexDrawing"))
-	{
-		HttpServerCD httpServer;
-		httpServer.run();
-	}
-	else
-	{
-		HttpServerRePAIR httpServer;
-		httpServer.run();
-	}
-	*/
-
+	
 	HTTPServer httpServer;
 	httpServer.run();
 
