8cb83d4b20aca17ea4a44444befeb90a1dba6a67
Yaniv Ohayon <yanivohayon8@gmail.com>
2024-07-11 15:32:41 +0300

    delete unused file

--- include/ScriptInputParser.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-#include <iostream>
-#include <map>
-
-//void parseInput(bool& oIsSimulationAuto, std::string &oPuzzleDirectory, int argc, char** argv);
-void parseInput(std::map<std::string,std::string>& oParam2Value, int argc, char** argv);
